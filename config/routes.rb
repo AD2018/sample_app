d247bee4b5e43fc4f671c97a33707b229c1c4df4
Al`mir <darzemanov668@gmail.com>
2017-05-16 18:19:26 +0300

    Finish lyaout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root              'static_pages#home'
   get  'contact' => 'static_pages#contact'
   get  'about'   => 'static_pages#about'
   get  'help'    => 'static_pages#help'
+  get  'signup'  => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
